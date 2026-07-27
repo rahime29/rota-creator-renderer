4f5a043964ac23523c91de6bd4b45593659f71fd
Rahime Yılmaz <46817291+rahime29@users.noreply.github.com>
2026-07-27 21:24:07 +0300

    Add TravelVideo component

---
+++ src/TravelVideo.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { AbsoluteFill } from "remotion";
+
+export const TravelVideo = () => {
+  return (
+    <AbsoluteFill
+      style={{
+        backgroundColor: "#000",
+        justifyContent: "center",
+        alignItems: "center",
+        color: "#fff",
+        fontSize: 64,
+        fontWeight: "bold",
+      }}
+    >
+      Rota Creator
+    </AbsoluteFill>
+  );
+};
